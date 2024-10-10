cd1ede345424b9a18b0b3f339d1c5b1f7ace6d59
Bandit <ilijadokic045@gmail.com>
2024-10-10 18:12:09 +0200

    Add project files.

---
+++ Texture.cpp
@@ -0,0 +1,52 @@
+#include "Texture.h"
+
+Texture::Texture(const char* image, GLenum texType, GLenum slot, GLenum format, GLenum pixelType)
+{
+	type = texType;
+
+	int widthImg, heightImg, numColCh;
+	// Becouse stb load image from bottom left corner
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* bytes = stbi_load(image, &widthImg, &heightImg, &numColCh, 0);
+
+	glGenTextures(1, &ID);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(texType, ID);
+
+	glTexParameteri(texType, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(texType, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glTexParameteri(texType, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(texType, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	//float flatColor[] = {1.0f, 1.0f, 1.0f, 1.0f};
+	//glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, flatColor);
+
+	glTexImage2D(texType, 0, GL_RGBA, widthImg, heightImg, 0, format, pixelType, bytes);
+	glGenerateMipmap(texType);
+
+	stbi_image_free(bytes);
+	glBindTexture(texType, 0);
+}
+
+void Texture::texUnit(Shader& shader, const char* uniform, GLuint unit)
+{
+	GLuint texUni = glGetUniformLocation(shader.ID, "tex0");
+	shader.Activate();
+	glUniform1i(texUni, unit);
+}
+
+void Texture::Bind()
+{
+	glBindTexture(type, ID);
+}
+
+void Texture::Unbind()
+{
+	glBindTexture(type, 0);
+}
+
+void Texture::Delete()
+{
+	glDeleteTextures(1, &ID);
+}
---
+++ main.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "Texture.h"
+#include "shaderClass.h"
+#include "VBO.h"
+#include "VAO.h"
+#include "EBO.h"
+
+const unsigned int width = 800;
+const unsigned int height = 800;
+
+// Verteces cordinates
+GLfloat vertices[] =
+{
+//  |      CORDINATES       |           COLOR         |
+	-0.5f, -0.5f , 0.0f,         1.0f,  0.0f,  0.0f,        0.0f, 0.0f,   // Lower left corner 0
+	 0.5f, -0.5f , 0.0f,         0.0f,  1.0f,  0.0f,        1.0f, 0.0f,   // Lower right corner 1
+	 0.5f,  0.5f , 0.0f,         0.0f,  0.0f,  1.0f,        1.0f, 1.0f,   // Upper right corner 2
+	-0.5f,  0.5f , 0.0f,         1.0f,  1.0f,  0.0f,        0.0f, 1.0f    // Upper left corner 3
+};
+
+// Indices for verteces order
+GLuint indices[] =
+{
+	0, 1, 3, // Left triangle
+	1, 2, 3  // Right triangle
+};
+
+
+
+int main()
+{
+	// Initialize GLFW
+	glfwInit();
+
+	// Tell GLFW what version of OpenGL we are using 
+	// In this case we are using OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it "Hello World!"
+	GLFWwindow* window = glfwCreateWindow(width, height, "Hello World!", NULL, NULL);
+	// Error check if the window fails to create
+	if (window == NULL) {
+
+		std::cout << "Failed to create window!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);
+
+	glfwSwapInterval(1);
+
+	//Load GLAD so it configures OpenGL
+	gladLoadGL();
+
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+	glViewport(0,0, width, height);
+
+	// Generates Shader object using shaders defualt.vert and default.frag
+	Shader shaderProgram("default.vert", "default.frag");
+
+	// Generates Vertex Array Object and binds it
+	VAO VAO1;
+	VAO1.Bind();
+
+	// Generates Vertex Buffer Object and links it to vertices
+	VBO VBO1(vertices, sizeof(vertices));
+	// Generates Element Buffer Object and links it to indices
+	EBO EBO1(indices, sizeof(indices));
+
+	// Links VBO attributes such as cordinates and colors to VAO
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	// Unbind all to prevent accidentally modifying them
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
+
+	// Get ID of uniform called "scale"
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID,"scale");
+
+	// Textures
+	Texture popCat("pop_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	popCat.texUnit(shaderProgram, "tex0", 0);
+
+	// Main while loop
+	while(!glfwWindowShouldClose(window))
+	{	
+		// Specify the color of the background
+		glClearColor(0.8f, 0.1f, 0.7f, 1.0f);
+		// Clean the back buffer and assign the new color to it
+		glClear(GL_COLOR_BUFFER_BIT);
+		// Tell OpenGL which Shader Program we want to use 
+		shaderProgram.Activate();
+
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj = glm::mat4(1.0f);
+
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -2.0f));
+		proj = glm::perspective(glm::radians(45.0f), (float)(width / height), 0.1f, 100.0f);
+
+		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
+		// Assigns a value to the uniform; NOTE Must be done always done after glUseProgram/ .Activate()
+		glUniform1f(uniID, 1.0f);
+		// Binding texture so its appear at render
+		popCat.Bind();
+		// Bind the VAO so OpenGL knows to use it
+		VAO1.Bind();
+		// Draw the triangle using the GL_TRIANGLES primitive
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
+		// Take care of all GLFW events
+		glfwPollEvents();
+	}
+
+	// Delete all the objects we've created
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	popCat.Delete();
+	shaderProgram.Delete();
+	// Delete window before ending the program
+	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
+	glfwTerminate();
+	return 0;
+}
